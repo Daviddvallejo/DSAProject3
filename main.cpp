4ec36e2d3d2b1f257b01a322afcafe974eb34748
FrancoiseHayek <fhayek@ufl.edu>
2023-04-25 18:55:30 -0400

    Wrote:      inputting all read values into the map      search for values timeframe away from input value and calculated the average and percent change      output line

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -23,32 +24,103 @@ int main(){
     // Map setup
     /*
      * Create map and store:
+     *
      *      1) Days when price was below current price
      *      2) Days when price was at current price (tolerance of 0.5%)
      *      3) Days when price was above current price
      *
+     * Keys:
+     *       "Down" when price is below tolerance
+     *       "At" when price is within tolerance
+     *       "Up" when price is above tolerance
+     * Values: A vector of pairs containing the day and the value
      */
-    unordered_map<int, vector<int>> myMap;
+    unordered_map<string, vector<pair<int, int>>> myMap;
 
     // Create input stream object, read in values and add to map accordingly
     ifstream inFS;
     inFS.open(file);
 
+    int upperBound = ceil(stockVal + (stockVal / 200));
+    int lowerBound = ceil(stockVal - (stockVal / 200));
     int day, val;
     string token;
     while (inFS) {
         inFS.clear();
 
+        // Read line and separate values into usable variables
         getline(inFS, token, ',');
         day = stoi(token);
         getline(inFS, token);
         val = stoi(token);
+
+        // Insert values into map
+        if (val < lowerBound) {
+            myMap["Down"].emplace_back(day, val);
+        } else if (val > upperBound) {
+            myMap["Up"].emplace_back(day, val);
+        } else {
+            myMap["At"].emplace_back(day, val);
+        }
+
     }
 
+    inFS.close();
+
+    // Loop through days in "At" and find day either in "Down", "At", or "Up" that is exactly timeframe away from day in "At"
+    // If/when a match is found, update average price.
+    // Keep track of num times price went up, down, or stayed the same and also number of times a day timeframe away was looked for.
 
 
+    // FIXME: CHANGE INNER FOR-LOOPS TO BINARY SEARCHES FOR DAYS TIMEFRAME AWAY
+    double totalSum = 0;
+    int matchedDays = 0;
+    for (auto i : myMap["At"]) {
+        for (auto x : myMap["Down"]) {
+            if (x.first - i.first == timeFrame) {
+                totalSum += x.second;
+                matchedDays++;
+                break;
+            }
+            if (x.first - i.first > timeFrame) {
+                break;
+            }
+        }
+        for (auto y : myMap["At"]){
+            if (y.first - i.first == timeFrame) {
+                totalSum += y.second;
+                matchedDays++;
+                break;
+            }
+            if (y.first - i.first > timeFrame) {
+                break;
+            }
+        }
+        for (auto z : myMap["Up"]) {
+            if (z.first - i.first == timeFrame) {
+                totalSum += z.second;
+                matchedDays++;
+                break;
+            }
+            if (z.first - i.first > timeFrame) {
+                break;
+            }
+        }
+    }
 
+    // Calculate average for price of stock after timeframe for every instance the stock was at the current price and
+    // the percent change
+    double avgPrice = totalSum / matchedDays;
+    double percentChange = (avgPrice - stockVal) / stockVal;
 
+    // FIXME: WRITE FORMULA FOR PERCENT CONFIDENCE
+    double confidence = 0;
+
+    // Output results to screen
+    cout << "The stock will change by " << percentChange << " in " << timeFrame << " trading days with a ";
+    cout << confidence << "% certainty" << endl;
+
+    // FIXME: END CLOCK FOR MAP HERE
 
 
 
